32c31342eee046623aa8dd5d9449f2785de8ed38
AnastasiyaP <pelevinaav@mail.ru>
2023-02-25 01:05:07 +0300

    add red task_1

--- Seminar2hw/task_1.py
+++ Seminar2hw/task_1.py
@@ -9,12 +9,12 @@ n = int(input())
 count_zero = 0
 count_one = 0
 for i in range(n):
-x = int(input())
+    x = int(input())
 if x == 0:
-count_zero += 1
+    count_zero += 1
 else:
-count_one += 1
+    count_one += 1
 if count_one > count_zero:
-print(count_zero)
+    print(count_zero)
 else:
-print(count_one)
+    print(count_one)
